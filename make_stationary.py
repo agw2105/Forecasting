33b44e169ed4169a53b39da2f8c459f94febbdaa
agw2105 <48716562+agw2105@users.noreply.github.com>
2019-11-05 13:32:26 -0500

    Bug fixes, added comments

--- make_stationary.py
+++ make_stationary.py
@@ -3,10 +3,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
-from DickeyFuller import test_stationarity
+import DickeyFullerTest as dft #custom module
 
-data = pd.read_csv("data.csv") #import timeseries data
-test_stationarity(data) #test for stationarity
+data = pd.read_csv("data.csv") #import timeseries data. Replace dummy data file with real time series
+dft.test_stationarity(data) #test for stationarity
 
 #Differencing to make data stationary
 data_log = np.log(data)
@@ -15,7 +15,7 @@ data_diff = data_diff.fillna(0) #first value is NaN due to shift function. The d
 plt.plot(data_diff)
 plt.show()
 
-test_stationarity(data_diff)
+dft.test_stationarity(data_diff)
 
 #Decomposition to make data stationary
 from statsmodels.tsa.seasonal import seasonal_decompose
@@ -29,4 +29,4 @@ fig = decomposition.plot()
 plt.show()
 
 residual.dropna(inplace=True)
-test_stationarity(residual)
+dft.test_stationarity(residual)
