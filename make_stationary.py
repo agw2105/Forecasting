ae65d004bb44ea800191a3947c98af9d01460638
agw2105 <48716562+agw2105@users.noreply.github.com>
2019-11-05 13:35:53 -0500

    Update make_stationary.py
    definitions, made more modular

--- make_stationary.py
+++ make_stationary.py
@@ -1,32 +1,31 @@
 #make data stationary
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import DickeyFullerTest as dft #custom module
 
-data = pd.read_csv("data.csv") #import timeseries data. Replace dummy data file with real time series
-dft.test_stationarity(data) #test for stationarity
-
 #Differencing to make data stationary
-data_log = np.log(data)
-data_diff = data_log - data_log.shift()
-data_diff = data_diff.fillna(0) #first value is NaN due to shift function. The differenced log values should be close to zero, so impute here
-plt.plot(data_diff)
-plt.show()
+def differencing(data):
+  data_log = np.log(data)
+  data_diff = data_log - data_log.shift()
+  data_diff = data_diff.fillna(0) #first value is NaN due to shift function. The differenced log values should be close to zero, so impute here
+  plt.plot(data_diff)
+  plt.show()
 
-dft.test_stationarity(data_diff)
+  dft.test_stationarity(data_diff)
 
 #Decomposition to make data stationary
 from statsmodels.tsa.seasonal import seasonal_decompose
-decomposition = seasonal_decompose(data_log)
+def decompose(data):
+  data_log = np.log(data)
+  decomposition = seasonal_decompose(data_log)
 
-trend = decomposition.trend
-seasonal = decomposition.seasonal
-residual = decomposition.resid
+  trend = decomposition.trend
+  seasonal = decomposition.seasonal
+  residual = decomposition.resid
 
-fig = decomposition.plot()
-plt.show()
+  fig = decomposition.plot()
+  plt.show()
 
-residual.dropna(inplace=True)
-dft.test_stationarity(residual)
+  residual.dropna(inplace=True)
+  dft.test_stationarity(residual)
